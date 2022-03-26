15d1fb564d47ca124608316bce0b32a54ee81f63
TarasovKiller <x.tarasov2013@yandex.ru>
2022-03-26 20:43:25 +0300

    try again

--- sheet.php
+++ sheet.php
@@ -6,11 +6,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 date_default_timezone_set('Europe/Moscow');
 
 $email = "sheetsphp@btcproject-345219.iam.gserviceaccount.com";
-$api_key = "9907c9ea7638c45e2eea395813b8b1ed5a89340c085b21a4a1a10cdb11e78220";
-$url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR,RUB&api_key=$api_key";
+$url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR,RUB";
 $info = file_get_contents("$url");
-print(gettype($info));
-print("\n");
 $info = json_decode($info,true);
 
 
