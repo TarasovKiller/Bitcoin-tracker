24e262321917885d761fa941064f57dcdafba026
TarasovKiller <x.tarasov2013@yandex.ru>
2022-03-26 19:33:50 +0300

    inital commit

---
+++ sheet.php
@@ -0,0 +1,81 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+date_default_timezone_set('Europe/Moscow');
+
+$email = "sheetsphp@btcproject-345219.iam.gserviceaccount.com";
+$api_key = "9907c9ea7638c45e2eea395813b8b1ed5a89340c085b21a4a1a10cdb11e78220";
+$url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR,RUB&api_key=$api_key";
+$info = file_get_contents("$url");
+print(gettype($info));
+print("\n");
+$info = json_decode($info,true);
+
+
+$spreadsheetId = "1O1Ws-LG68NrJ6NNxdIsvCvnFEi5WihZPL2gT9fmnogg";
+$sheetid = "0";
+$googleAccountKeyFilePath = __DIR__ . '/service_key.json';
+putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $googleAccountKeyFilePath);
+
+
+$client = new Google_Client();
+$client->useApplicationDefaultCredentials();
+$client->addScope('https://www.googleapis.com/auth/spreadsheets');
+$service = new Google_Service_Sheets($client);
+$response = $service->spreadsheets->get($spreadsheetId);
+$sheetProperties = $response->getSheets()[0]->getProperties();
+
+print($sheetProperties->title);
+
+$rows = $sheetProperties->getGridProperties()->rowCount;
+
+//Range Reading
+// $range = "List 1!A2:D4";
+// $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+// $values = $response->getValues();
+// if(empty($values)) {
+//     print "No date\n";
+// }else {
+//     $mask = "%10s %-10s %s\n";
+//     foreach ($values as $row) {
+//         echo sprintf($mask, $row[3],$row[2],$row[1]);
+//     }
+// }
+// print($rows);
+
+
+
+// Range Updating
+
+$time_now = date('d.m.Y H:i:s');
+$range = "List 1";
+$values = [
+    [$time_now,$info["RUB"],$info["USD"],$info["EUR"]],
+];
+$body = new Google_Service_Sheets_ValueRange([
+    'values' => $values
+]);
+$params = [
+    'valueInputOption' => 'RAW'
+];
+$insert = [
+    "insertDataOption" => "INSERT_ROWS"
+];
+$result = $service->spreadsheets_values->append(
+    $spreadsheetId,
+    $range,
+    $body,
+    $params,
+    $insert
+);
+
+
+// Идентификатор таблицы
+// $csv = file_get_contents('https://docs.google.com/spreadsheets/d/' . $spreadsheetId . '/export?format=csv&gid=' . $sheetid);
+// $csv = explode("\r\n", $csv);
+// $array = array_map('str_getcsv', $csv);
+
+// print_r($array);
+?>
---
+++ site.php
@@ -0,0 +1,28 @@
+
+<?php
+    // $w = stream_get_wrappers();
+    // echo 'openssl: ',  extension_loaded  ('openssl') ? 'yes':'no', "\n";
+    // echo 'http wrapper: ', in_array('http', $w) ? 'yes':'no', "\n";
+    // echo 'https wrapper: ', in_array('https', $w) ? 'yes':'no', "\n";
+    // echo 'wrappers: ', var_dump($w);
+    // $characterName = "John";
+    // $characterAge = 34;
+    // $email = "sheetsphp@btcproject-345219.iam.gserviceaccount.com";
+    // $api_key = "9907c9ea7638c45e2eea395813b8b1ed5a89340c085b21a4a1a10cdb11e78220";
+    // $url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR,RUB&api_key=$api_key";
+    // $info = file_get_contents("$url");
+    // print(gettype($info));
+    // print("\n");
+    // $info = json_decode($info,true);
+
+    // print(gettype($info));
+    // print("\n");
+    // print_r($info);
+    date_default_timezone_set('Europe/Moscow');
+    $a = date('d.m.Y H:i:s');
+    echo $a;
+
+
+
+    // phpinfo();
+?>
